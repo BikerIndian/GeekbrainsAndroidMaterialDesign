0af7e05a042204d97826e0cea030a9f78f4c3d5f
Vladimir Svishch <5693031@gmail.com>
2021-04-07 04:35:08 +0300

    Урок 6. RecyclerView Добавьте в своё приложение экран со списком, в котором пользователь сможет создавать заметки, редактировать их или перемещать относительно друг друга. Примените для этого полученные на уроке знания.

--- app/src/main/java/net/svishch/android/pictureoftheday/ui/viewPager/MarsFragment.kt
+++ app/src/main/java/net/svishch/android/pictureoftheday/ui/viewPager/MarsFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.bottom_sheet_layout.*
 import kotlinx.android.synthetic.main.fragment_mars.*
 import kotlinx.android.synthetic.main.item_mars_photos.*
@@ -14,11 +16,13 @@ import net.svishch.android.outerspace.ui.adapter.MarsPhotosRVAdapter
 import net.svishch.android.pictureoftheday.R
 import net.svishch.android.pictureoftheday.apiNasa.mars.MarsPhotoData
 import net.svishch.android.pictureoftheday.apiNasa.mars.MarsViewModel
+import net.svishch.android.pictureoftheday.ui.viewPager.adapter.ItemTouchHelperCallback
+import net.svishch.android.pictureoftheday.ui.viewPager.adapter.OnStartDragListener
 
 
-class MarsFragment : Fragment() {
+class MarsFragment : Fragment(),OnStartDragListener {
 
-
+    private lateinit var itemTouchHelper: ItemTouchHelper
     private val viewModel: MarsViewModel by lazy {
         ViewModelProviders.of(this).get(MarsViewModel::class.java)
     }
@@ -26,7 +30,7 @@ class MarsFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel.getMarsData()
-                .observe(this@MarsFragment, { renderData(it) })
+            .observe(this@MarsFragment, { renderData(it) })
 
         rv_mars_photos.layoutManager = LinearLayoutManager(context)
 
@@ -38,8 +42,8 @@ class MarsFragment : Fragment() {
     }
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?,
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
     ): View? {
 
         return inflater.inflate(R.layout.fragment_mars, container, false)
@@ -49,9 +53,19 @@ class MarsFragment : Fragment() {
         when (data) {
             is MarsPhotoData.Success -> {
                 val serverResponseData = data.serverResponseData
-                rv_mars_photos.adapter = serverResponseData.getPhotos()?.let { MarsPhotosRVAdapter(it) }
+                val adapter = serverResponseData.getPhotos()?.let {
+                    MarsPhotosRVAdapter(it,this)
+                }
+                rv_mars_photos.adapter = adapter
+
+                adapter?.let { itemTouchHelper = ItemTouchHelper(ItemTouchHelperCallback(it)) }
+                itemTouchHelper.attachToRecyclerView(rv_mars_photos)
             }
 
         }
+    }
+
+    override fun onStartDrag(viewHolder: RecyclerView.ViewHolder) {
+        itemTouchHelper.startDrag(viewHolder)
     }
 }
--- app/src/main/java/net/svishch/android/pictureoftheday/ui/viewPager/adapter/MarsPhotosRVAdapter.kt
+++ app/src/main/java/net/svishch/android/pictureoftheday/ui/viewPager/adapter/MarsPhotosRVAdapter.kt
@@ -1,11 +1,10 @@
 package net.svishch.android.outerspace.ui.adapter
 
+import android.graphics.Color
 import android.transition.Slide
-import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ImageView
+import androidx.core.view.MotionEventCompat
 import androidx.recyclerview.widget.RecyclerView
 import androidx.transition.ChangeBounds
 import androidx.transition.ChangeImageTransform
@@ -16,10 +15,18 @@ import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.item_mars_photos.view.*
 import net.svishch.android.pictureoftheday.R
 import net.svishch.android.pictureoftheday.apiNasa.mars.entity.Photo
+import net.svishch.android.pictureoftheday.ui.viewPager.adapter.ItemTouchHelperAdapter
+import net.svishch.android.pictureoftheday.ui.viewPager.adapter.ItemTouchHelperViewHolder
+import net.svishch.android.pictureoftheday.ui.viewPager.adapter.OnStartDragListener
 
 
-class MarsPhotosRVAdapter(private val values: List<Photo>) :
-    RecyclerView.Adapter<MarsPhotosRVAdapter.ViewHolder>() {
+class MarsPhotosRVAdapter(
+    private var data: List<Photo>,
+    private val dragListener: OnStartDragListener
+) :
+    RecyclerView.Adapter<MarsPhotosRVAdapter.ViewHolder>(), ItemTouchHelperAdapter {
+    private val values = data.toMutableList()
+
 
     // Размер списка
     override fun getItemCount() = values.size
@@ -35,15 +42,21 @@ class MarsPhotosRVAdapter(private val values: List<Photo>) :
 
         holder.setInfo(values[position].earthDate.toString())
         holder.setImg(values[position].imgSrc.toString())
+        holder.setInfoPhoto(values[position])
 
-        holder.containerView.setOnClickListener {
+        holder.containerView.iv_img.setOnClickListener {
             zoom(holder)
         }
     }
 
-    class ViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView),
-        LayoutContainer {
+
+    inner class ViewHolder(override val containerView: View) :
+        RecyclerView.ViewHolder(containerView),
+        LayoutContainer, ItemTouchHelperViewHolder {
+        var cardBackgroundColor = 0
         var isExpanded = false
+        var isInfo = false
+
         fun setInfo(text: String) = with(containerView) {
             tv_info.text = text
         }
@@ -55,6 +68,70 @@ class MarsPhotosRVAdapter(private val values: List<Photo>) :
             }
         }
 
+        fun setInfoPhoto(photo: Photo) = with(containerView) {
+            marsInfoPhoto.text = String.format(
+                "Photo id: %s \n\n" +
+                        "Camera id: %s  \n" +
+                        "Camera name: %s  \n" +
+                        "Camera full name: %s  \n\n" +
+                        "Rover id: %s  \n" +
+                        "Rover name: %s  \n" +
+                        "Rover landing date: %s  \n" +
+                        "Rover launch date: %s  \n" +
+                        "Rover status: %s  \n",
+                photo.id,
+                photo.camera?.id,
+                photo.camera?.name,
+                photo.camera?.fullName,
+                photo.rover?.id,
+                photo.rover?.name,
+                photo.rover?.landingDate,
+                photo.rover?.launchDate,
+                photo.rover?.status,
+            )
+        }
+
+        override fun onItemSelected() {
+            cardBackgroundColor = itemView.iv_LinearLayout.cardBackgroundColor.defaultColor
+            itemView.iv_LinearLayout.setCardBackgroundColor(Color.LTGRAY)
+        }
+
+        override fun onItemClear() {
+            itemView.iv_LinearLayout.setCardBackgroundColor(cardBackgroundColor)
+        }
+
+
+        init {
+            containerView.removeItemImageView.setOnClickListener { removeItem() }
+            containerView.dragHandleImageView.setOnTouchListener { _, event ->
+                if (MotionEventCompat.getActionMasked(event) == MotionEvent.ACTION_DOWN) {
+                    dragListener.onStartDrag(this)
+                }
+                false
+            }
+            setInfoClick()
+        }
+
+        fun removeItem() {
+            values.removeAt(layoutPosition)
+            notifyItemRemoved(layoutPosition)
+        }
+
+        fun setInfoClick() {
+            containerView.img_mars_photo_info.visibility = View.GONE
+
+            containerView.img_mars_photo_info.setOnClickListener {
+                android.transition.TransitionManager.beginDelayedTransition(
+                    containerView.iv_LinearLayout,
+                    Slide(Gravity.END).setDuration(600L)
+                )
+
+                isInfo = !isInfo
+                containerView.marsInfoPhoto.visibility =
+                    if (isInfo) View.VISIBLE else View.GONE
+
+            }
+        }
     }
 
     private fun zoom(holder: ViewHolder) {
@@ -70,17 +147,44 @@ class MarsPhotosRVAdapter(private val values: List<Photo>) :
                 .addTransition(ChangeImageTransform())
         )
 
+
+        if (holder.isExpanded) {
+            holder.containerView.img_mars_photo_info.visibility = View.VISIBLE
+            holder.containerView.dragHandleImageView.visibility = View.GONE
+            holder.containerView.removeItemImageView.visibility = View.GONE
+
+        } else {
+            holder.containerView.img_mars_photo_info.visibility = View.GONE
+            holder.containerView.marsInfoPhoto.visibility = View.GONE
+            holder.containerView.dragHandleImageView.visibility = View.VISIBLE
+            holder.containerView.removeItemImageView.visibility = View.VISIBLE
+        }
         img.scaleType =
-            if (holder.isExpanded) ImageView.ScaleType.CENTER_CROP else
+            if (holder.isExpanded) {
+                ImageView.ScaleType.CENTER_CROP
+            } else {
                 ImageView.ScaleType.FIT_CENTER
+            }
 
         // Анимация текста
         android.transition.TransitionManager.beginDelayedTransition(
-            holder.containerView.mars_info,
+            holder.containerView.iv_LinearLayout,
             Slide(Gravity.END).setDuration(600L)
         )
         holder.containerView.tv_info.visibility = if (holder.isExpanded) View.VISIBLE else View.GONE
 
+    }
+
+    override fun onItemMove(fromPosition: Int, toPosition: Int) {
+        values.removeAt(fromPosition).apply {
+            values.add(if (toPosition > fromPosition) toPosition - 1 else toPosition, this)
+        }
+        notifyItemMoved(fromPosition, toPosition)
+    }
+
+    override fun onItemDismiss(position: Int) {
+        values.removeAt(position)
+        notifyItemRemoved(position)
     }
 
 }
